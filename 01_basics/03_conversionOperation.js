7fb75c026f01f9a8d8ed1ba52564a7ea5689c468
Zorain Kazmi <zorainkazmi@gmail.com>
2024-01-21 19:48:28 +0500

    conversion and Operation completed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,98 @@
+// Number 
+let score = "33" // output = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// Contersion of Number
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0; undefined => NaN
+// "Zain" => NaN
+
+
+// Boolean 
+let isLoggedIn = 1  // output = true
+
+let booleanIsLogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLogedIn);
+
+// conversion of Boolean
+// 1 => true; 0 => false
+// "" => false
+// "Zain" => true
+// null => false, undefined => false
+
+
+// String
+
+
+
+//  conversion of string
+someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// conversion of String
+// 33 => "33"
+// true => "true", false => "false"
+// null => "null", undefined => "undefined"
+
+
+
+// ************************************ Operations***************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = "Zain"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(true)
+// console.log(+true)
+// console.log(true+) // error 
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;  // postfix
+// ++gameCounter; // prefix
+// console.log(gameCounter);
+
+/*
+link to study prefix and postfix 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+*/
+
+
+
+
