f89e68bfd8186c4f935d19ba8d24d5cb9120c615
Zorain Kazmi <zorainkazmi@gmail.com>
2024-01-21 20:33:02 +0500

    comparision completed

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,32 @@
+// console.log(2 > 2);
+// console.log(2 < 2);
+// console.log(2 >= 2);
+// console.log(2 <= 2);
+// console.log(2 == 2);
+// console.log(2 === 2);
+// console.log(2!= 2);
+// console.log(2!== 2);
+
+
+// console.log("2" > 1);  // output = true
+// console.log("02" > 1);  // output = true
+
+// console.log(null > 0);  // false
+// console.log(null == 0); // false
+// console.log(null >= 0);  // true
+// console.log(null <= 0);  // true
+// -----------------------------------
+// console.log(undefined > 0);  // false
+// console.log(undefined == 0); // false
+// console.log(undefined >= 0);  // false
+// console.log(undefined <= 0);  // false
+
+
+// ===  -> strict check
+// ==  -> loose check
+// console.log( "2" === 2 )
+// console.log(undefined === 0); // false
+// console.log(null === 0); // false
+
+
+
