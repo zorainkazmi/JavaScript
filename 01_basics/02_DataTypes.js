60c57fa3ddd1c3126ccc95fb75204dd4efa75313
Zorain Kazmi <zorainkazmi@gmail.com>
2024-01-21 16:57:55 +0500

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 )  // we are using nodejs, not browser
+
+  console.log(3 
+    + 3); // output = 6 --> but code readability should be high
+
+
+ 
+    let name = "Zain"
+    let age = 19
+    let isLoggedIn = false
+    // let state;  // undefine
+    let state = null
+
+
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true, false
+    // null => no value => standalone value
+    // undefined => value not assign 
+    // symbol => unique
+
+ console.log(null == undefined); // true
+ console.log(null === undefined); // false
+     
+    // object
+
+    console.log(typeof "Zain") //string
+    console.log(typeof age) // number
+    console.log(typeof null);  // object
+    console.log(typeof undefined);  // undefine
+    
+ 
+
+
+ 
