2ab15bbfb033a4e9d270edc924356d8acf9d06c1
Zorain Kazmi <zorainkazmi@gmail.com>
2024-01-26 00:28:33 +0500

    nums and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,45 @@
+const score = 400
+// console.log(score)
+
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+
+const hundreds = 1000000  
+// console.log(hundreds.toLocaleString('en-PK'));
+
+
+// ++++++++++++++++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.max(10, 20));
+// console.log(Math.min(10, 20));
+// console.log(Math.pow(2, 3));
+// console.log(Math.sqrt(25));
+
+// console.log(Math.random()); //from 0 upto 1
+// console.log(Math.random() * 10); //from 0 upto 10
+// console.log((Math.random() * 10) + 1); //from 1 upto 10
+// console.log(Math.floor(Math.random() * 10) + 1); //from 1 upto 10
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
