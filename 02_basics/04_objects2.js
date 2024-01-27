c371e2a057f641da355364fa502858be92b357f7
Zorain Kazmi <zorainkazmi@gmail.com>
2024-01-27 21:47:20 +0500

    Added  demonstration of object properties, destructuring, and JSON API in object part 2.

--- 02_basics/04_objects2.js
+++ 02_basics/04_objects2.js
@@ -50,12 +50,44 @@ const users = [
 ]
 
 users[1].email   // when we access user[1] its mean object at index 1 in array and its email
-console.log(instagramUser);
+// console.log(instagramUser);
 
-console.log(Object.keys(instagramUser));
-console.log(Object.values(instagramUser));
-console.log(Object.entries(instagramUser));
+// console.log(Object.keys(instagramUser));
+// console.log(Object.values(instagramUser));
+// console.log(Object.entries(instagramUser));
 
-console.log(instagramUser.hasOwnProperty('isLoggedIn'));
+// console.log(instagramUser.hasOwnProperty('isLoggedIn'));
+
+
+
+// object de-structure and JSON API 
+
+const course = {
+    courseName: "JS with Zain",
+    price: "999",
+    courseInstructor: "Zain"
+}
+
+// course.courseInstructor
+
+// const {courseInstructor} = course
+const {courseInstructor: instructor} = course  // rename the key   
+// console.log(courseInstructor);
+console.log(instructor);
+
+//  API 
+
+// {
+//     "name": "Zain",
+//     "coursename": "JS with Zain",
+//     "price": "999",
+// }
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
+
 
 
