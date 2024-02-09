c0e2723d5d8ab0d0a35e37754033e6f18f76488d
Syed Zorain Ejaz Kazmi <139112596+zorainkazmi@users.noreply.github.com>
2024-02-10 01:07:54 +0500

    while loop, do while loop complete

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,27 @@
+
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ["flash", "batman", "spiderman"] 
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`)
+    arr = arr + 1
+}
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+let score1 = 11
+do {
+    console.log(`Score1 is ${score1}`);
+    score1++
+} while (score1 <= 10);
