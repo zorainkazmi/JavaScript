303298ff864af81bfbafc91dc28c58e22d9f9b13
Syed Zorain Ejaz Kazmi <139112596+zorainkazmi@users.noreply.github.com>
2024-02-10 01:09:56 +0500

    for IN loop, map

---
+++ 05_iterations/04_forInloop.js
@@ -0,0 +1,60 @@
+
+// const myObject = {
+//     js: "javascript",
+//     cpp: "C++",
+//     rb: "Ruby"
+// }
+
+// for (const key in myObject) {
+    // console.log(myObject[key])
+    // console.log(`${key} shortcut is for ${myObject[key]}`)
+// }
+
+
+// const program = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in program) {
+    // console.log(key)
+// }
+
+// const map = new Map()
+// map.set("PAK", "Pakistan")
+// map.set("USA", "United States of America")
+// map.set("Fr", "France")
+
+
+// for (const key in map) {
+//     console.log(key)
+// }
+
+
+
+
+
+
+// ***************************************************************************************
+
+// const myObject = {
+//     js: "javascript",
+//     cpp: "C++",
+//     rb: "Ruby"
+// }
+// for (const key in myObject) {
+    // console.log(key)
+    // console.log(myObject)
+    // console.log(myObject[key])
+    // console.log(`${key} shortcut is for ${myObject[key]}`)
+// }
+
+
+
+// const myArray = ["js", "ruby", "python", "java", "cpp"]
+// for (const key in myArray) {
+    // console.log(key)
+    // console.log(myArray)
+    // console.log(myArray[key])
+    // console.log(`${key} is index of ${myArray[key]}`)
+// }
+
+
+
